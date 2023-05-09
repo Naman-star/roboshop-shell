3aa237e4505aeb51a9ae23433236401754c6607a
user <namanshetty91132@gmail.com>
2023-05-09 08:31:32 +0100

    naman

--- common.sh
+++ common.sh
@@ -31,7 +31,7 @@ func_schema_setup() {
     func_stat_check $?
 
     func_print_head "Load Schema"
-    mongo --host mongodb-dev.ndevopsb72.online </app/schema/${ component }.js &>>$log_file
+    mongo --host mongodb-dev.ndevopsb72.online </app/schema/${component}.js &>>$log_file
     func_stat_check $?
   fi
   if [ "${schema_setup}" == "mysql" ]; then
